46ed789cdb220755e408da5277e73badc35f2e7e
Dain Vugs <dain.vugs@rocva.nl>
2021-01-22 16:16:15 +0100

    opdracht

---
+++ 05 - Javascript/taak01 - Console.log/script.js
@@ -0,0 +1,1 @@
+console.log("Mijn naam is Dain en dit is mijn eerste console bericht");
---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,6 @@
+let voornaam = "Dain"
+let achternaam = "Vugs"
+let leeftijd = 18
+let ikMagLegaalAlcoholKopen = "ja"
+let ikMagBrommerRijden = "nee"
+let SoftwareOntwikkeling = "ja"
